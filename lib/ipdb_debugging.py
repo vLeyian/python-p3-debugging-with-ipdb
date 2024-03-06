4d5465a90fb9f95ca2ee07a4dd47691ca3fe45f2
Leyian <victor.leyian@student.moringaschool.com>
2024-03-06 03:42:58 +0300

    completed

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,5 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
-    return num
+    result = num + 2  
+    return result  
--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import ipdb
 
 def tracing_the_function():
